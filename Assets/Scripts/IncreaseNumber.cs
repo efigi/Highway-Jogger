47f8bdb681be26014c64a6f6436531c92b2499bb
Benjo Kho <benjokho@gmail.com>
2020-10-16 00:34:31 +0800

    Increase score

---
+++ Assets/Scripts/IncreaseNumber.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IncreaseNumber : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Text textToIncrease;
+    public float currentValue;
+    public float increment;
+    public float setTime;
+    private float counter;
+
+    private void Reset()
+    {
+        setTime = 1.0f;
+        currentValue = 0.0f;
+    }
+    void Start()
+    {
+        counter = setTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        counter -= Time.deltaTime;
+        if (counter <= 0)
+        {
+            counter = setTime;
+            currentValue += increment;
+            textToIncrease.text = currentValue.ToString();
+        }
+    }
+}
